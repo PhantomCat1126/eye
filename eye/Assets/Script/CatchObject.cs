fe1d22493a18453986e40d4afe93acbc38f32bef
PhantomCat1126 <31592655+PhantomCat1126@users.noreply.github.com>
2021-11-17 15:32:06 +0800

    no message

---
+++ eye/Assets/Script/CatchObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchObject : MonoBehaviour {
+
+	Rigidbody r;
+	Vector3 OriginalPos;
+	public GameObject AnswerPosition;       // 目標位置 
+
+	bool isCilck = false;
+	bool isTarget = false;
+
+	void Awake()
+	{
+		r = this.GetComponent<Rigidbody>();
+		OriginalPos = this.GetComponent<Transform>().position;
+		//AnswerPos = AnswerPosition.GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Collider>().tag == AnswerPosition.GetComponent<Collider>().tag)
+		{
+			GameControl.Instance.inGoal();
+			//print("inGoalNum: " + GameControl.inGoalNum);
+			transform.position = AnswerPosition.GetComponent<Transform>().position;
+		}
+	}
+
+	/// <summary>
+	/// 當滑鼠按鍵放開時，可觸發此事件。
+	/// </summary>
+	void OnMouseUp()
+	{
+		isCilck = false;
+	}
+
+	//確認選取
+	/// <summary>
+	/// 當滑鼠游標已移入觸發區域並且尚未移出觸發區域前，會"持續"觸發此事件
+	/// </summary>
+	void OnMouseOver()
+	{
+		isTarget = true;
+		//print("isTarget is "+ isTarget);
+
+		if (Input.GetMouseButton(0)) {
+			isCilck = true;
+			//Updatetransform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smooth);
+			print("按下滑鼠左鍵");
+		}
+
+	}
+	/// <summary>
+	/// 當滑鼠游標移入觸發區域時，可觸發此事件。
+	/// </summary>
+	void OnMouseExit()
+	{
+		isTarget = false;
+		//print("isTarget is " + isTarget);
+
+	}
+}
---
+++ eye/Assets/Script/GameControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+	public static GameControl Instance;
+
+	[HeaderAttribute("目標位置")]
+	public GameObject[] AnswerPosition;       // 目標位置 
+
+	[HeaderAttribute("可移動物件")]
+	public GameObject[] FollowMouse;
+
+	public static int inGoalNum = 0;
+
+	public GameObject UIButton;
+
+	// Use this for initialization
+	void Awake () {
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool ReturnBool(bool b) 
+	{
+		return b;
+	}
+
+	public void inGoal() 
+	{
+		inGoalNum += 1;
+		CheckGoal();
+	}
+
+	bool CheckGoal() 
+	{
+		if (inGoalNum == 0) return false;
+
+		if(inGoalNum == AnswerPosition.Length)
+        {
+			UIButton.SetActive(true);
+			return true;
+		}
+		return true;
+	}
+}
